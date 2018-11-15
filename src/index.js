887d217e7d6cdbca4b9d4b97631ed2148812241e
luwenyou <luwenyou@corp.netease.com>
2018-11-15 15:20:24 +0800

    移除之前自己写的react-redux,使用官方的react-redux

--- src/index.js
+++ src/index.js
@@ -4,20 +4,8 @@ import ReactDOM from 'react-dom'
 import Header from './Header'
 import Content from './Content'
 import './index.css'
-import { Provider } from './react-redux'
-
-function createStore (reducer) {
-  let state = null
-  const listeners = []
-  const subscribe = (listener) => listeners.push(listener)
-  const getState = () => state
-  const dispatch = (action) => {
-    state = reducer(state, action)
-    listeners.forEach((listener) => listener())
-  }
-  dispatch({}) // 初始化 state
-  return { getState, dispatch, subscribe }
-}
+import { Provider } from 'react-redux'
+import {createStore} from 'redux'
 
 const themeReducer = (state, action) => {
   if (!state) return {
